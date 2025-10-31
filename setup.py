783e0acd0187af9dcc5e001944f10fb8adb28f69
victor <vireks@terpmail.umd.edu>
2025-10-31 02:53:54 -0400

    add build files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='svc_viz',
-    version='0.1.4',
+    version='0.1.5',
     author='Victor Irekponor, Taylor Oshan',
     author_email='vireks@umd.edu',
     description='A python software package for visualizing the results of spatially varying coefficient (SVC) models, enhancing reproducibility and replicability',
